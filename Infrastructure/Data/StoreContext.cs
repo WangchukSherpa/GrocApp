1397195ce4911b0fdacd5b557441b5ef6afe4fc8
WangchukSherpa <b200044@nitsikkim.ac.in>
2024-09-21 01:33:02 +0530

    Innitial commit

---
+++ Infrastructure/Data/StoreContext.cs
@@ -0,0 +1,14 @@
+﻿
+using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options) : base(options)//connection string
+        {
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
